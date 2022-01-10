b18c8eadc392f9e87ce7fc8f47610c4498314c40
mohamedatef2020 <mohamed.atef.pd2020@gmail.com>
2022-01-10 22:10:24 +0200

    Add missing step

--- cypress/integration/AvailabilitiesCheck.feature
+++ cypress/integration/AvailabilitiesCheck.feature
@@ -2,4 +2,5 @@ Feature: Check availability of practitioners
 Scenario: Check availability
     Given I go to "/"
     When I search for Practitioner "Test QATECHNIQUE"
+    And I choose the reason "Ablation de fils"
     Then I see the availability
--- cypress/integration/AvailabilitiesCheck/step_definitions.js
+++ cypress/integration/AvailabilitiesCheck/step_definitions.js
@@ -1,4 +1,4 @@
-import { And, then, Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { And, Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('I go to {string}', url => {
     cy.visit(url);
@@ -10,9 +10,9 @@ When('I search for Practitioner {string}', name => {
     cy.get("#downshift_input_0").wait(1000).type('{downarrow}{enter}');
 });
 
-And('I choose a reason {string}', reason => {
+And('I choose the reason {string}', reason => {
     cy.get("#consultationReasonName").click();
-    cy.get(`[data-cy=${reason}]`).click();
+    cy.get(`[data-cy="${reason}"]`).click();
 });
 
 Then('I see the availability', () => {
